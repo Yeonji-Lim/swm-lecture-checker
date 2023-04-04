82365e9c95428cdf3c0c544113c1f69b755e9cf8
Yeonji-Lim <view7186@naver.com>
2023-04-04 18:13:38 +0900

    디스코드 알림봇 연동 및 스케줄 설정

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import json
 import requests
 from bs4 import BeautifulSoup as bs
+import schedule
+import time
+
+recent_lec = ''
 
 def job():
-
-    LOGIN_INFO = json.load(open('login_info.json', 'r'))
-
+    global recent_lec
     with requests.Session() as s:
+
+        print('최근 강의 : ', recent_lec)
+        json_data = json.load(open('info.json', 'r'))
+        LOGIN_INFO = json_data['LOGIN_INFO']
+        DISCORD_WEBHOOK_URL = json_data['DISCORD_WEBHOOK_URL']
 
         login_res = s.get('https://www.swmaestro.org/sw/member/user/forLogin.do?menuNo=200025')
         soup = bs(login_res.text, 'html.parser')
@@ -26,4 +33,22 @@ def job():
         login_res = s.post('https://www.swmaestro.org/sw/login.do', data=LOGIN_INFO)
         print('로그인 요청 결과 : ', login_res.status_code)
 
-job()
+        if login_res.ok:
+            mentolec_page = s.get('https://www.swmaestro.org/sw/mypage/mentoLec/list.do?menuNo=200046', verify=False)
+            print('멘토링 페이지 접근 결과 : ', mentolec_page.status_code)
+            if mentolec_page.ok:
+                soup = bs(mentolec_page.text, 'html.parser')
+                new_lec = soup.select_one('#listFrm > table > tbody > tr:nth-child(1) > td.tit > div.rel > a').attrs['href']
+                if recent_lec != '':
+                    if new_lec != recent_lec:
+                        recent_lec = new_lec
+                        message = {'content':'소마에 새로운 강의가 올라왔습니다!!'}
+                        requests.post(DISCORD_WEBHOOK_URL, data=message)
+                else:
+                    recent_lec = new_lec
+
+schedule.every(3).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
